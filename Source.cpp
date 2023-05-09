af34d8e1a8ce5cc0deda748fbe49fdd93b1a437f
zsolt2106 <zsolt2106@gmail.com>
2023-05-09 09:52:51 +0200

    feltúrbózott házi vonalmozgatással

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,8 @@
 #include <math.h>
 
 /*void display(GLFWwindow* window, double currentTime) {
-    glClearColor(1.8, 1.2, 0.5, 0.2);//fakó-sárga
-    //glClearColor(1.0, 1.0, 0.0, 0.0);//sárga
+    glClearColor(1.8, 1.2, 0.5, 0.2);//fako-sarga
+    //glClearColor(1.0, 1.0, 0.0, 0.0);//sarga
     //glClearColor(1.0, 0.0, 0.0, 1.0);//piros
     glClear(GL_COLOR_BUFFER_BIT);
 }*/
@@ -12,25 +12,74 @@
 //to-do -
 
 
-const int WS = 600; //Ablak mérete
+const int WS = 600; //Ablak size
 
 
-//KÖR
+//KOR
 const float RADIUS = (2.0f / float(WS)) * 50.0f;
 const float MOV_SPEED = 0.045f;
 const int STEPS = 350;
 const float ANGLE = 2.f * 3.14 / STEPS;
 
-//VONAL(KÉK)
+//VONAL(Kï¿½K)
 const float vonal_x = 1.f / 3.f;
 float vonal_y = 0.f;
 
 
 
-//mozgások
+//mozgï¿½sok
 float kor_x = 0.0f;
 float kor_y = 0.0f;
 float mozog = MOV_SPEED;
+
+int main() {
+    GLFWwindow* window;
+
+   #include <iostream>
+#include <GLFW/glfw3.h>
+#include <math.h>
+
+/*void display(GLFWwindow* window, double currentTime) {
+    glClearColor(1.8, 1.2, 0.5, 0.2);//fako-sarga
+    //glClearColor(1.0, 1.0, 0.0, 0.0);//sï¿½rga
+    //glClearColor(1.0, 0.0, 0.0, 1.0);//piros
+    glClear(GL_COLOR_BUFFER_BIT);
+}*/
+
+//to-do -
+
+
+const int WSize = 600; //Ablak size
+
+
+//KOR
+const float RADIUS = (2.0f / float(WSize)) * 50.0f;
+const float MOV_SPEED = 0.045f;
+const int STEPS = 350;
+const float ANGLE = 2.f * 3.14 / STEPS;
+
+//VONAL(KEK)
+const float vonal_x = 1.f / 3.f;
+float vonal_y = 0.f;
+
+
+
+//mozgasok
+float kor_x = 0.0f;
+float kor_y = 0.0f;
+float mozog = MOV_SPEED;
+
+//vonal mozgatÃ¡s
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_UP && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+        vonal_y += 0.01;
+    }
+    if (key == GLFW_KEY_DOWN && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+        vonal_y -= 0.01;
+    }
+}
+
+
 
 int main() {
     GLFWwindow* window;
@@ -40,7 +89,7 @@ int main() {
         return -1;
     }
 
-    window = glfwCreateWindow(WS, WS, "Ablak sarga haterrel", 0, 0);
+    window = glfwCreateWindow(WSize, WSize, "Ablak sarga haterrel", 0, 0);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -52,12 +101,12 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         GLfloat line[] = { -vonal_x, vonal_y, 0.f, vonal_x, vonal_y, 0.f };
 
-        glClearColor(1.8, 1.2, 0.5, 0.2);//fakó-sárga, also known as banán sárga;
+        glClearColor(1.8, 1.2, 0.5, 0.2);//fako-sarga, also known as banan sarga;
         glClear(GL_COLOR_BUFFER_BIT);
-        glfwSetWindowSizeLimits(window, WS, WS, WS, WS);
+        glfwSetWindowSizeLimits(window, WSize, WSize, WSize, WSize);
 
         kor_x += mozog;
-        if ((kor_x > 1-RADIUS) || (kor_x < -(1-RADIUS))) 
+        if ((kor_x > 1 - RADIUS) || (kor_x < -(1 - RADIUS)))
             mozog = -mozog;
 
         float prev_x = kor_x;
@@ -81,40 +130,41 @@ int main() {
         }
 
         glEnable(GL_LINE_SMOOTH);
-        glLineWidth(3.0); //Vastagásg fix 3
-        glColor4f(0.31, 0.10, 1.0, 0.31); //Esküszöm, hogy nem lila
+        glLineWidth(3.0); //Vastagsag fix 3
+        glColor4f(0.31, 0.10, 1.0, 0.31); //Eskuszom, hogy nem lila
         glEnableClientState(GL_VERTEX_ARRAY);
         glVertexPointer(3, GL_FLOAT, 0, line);
-        glDrawArrays(GL_LINES, 0, 2); //vonal kirajzolása
+        glDrawArrays(GL_LINES, 0, 2); //vonal kirajzol
         glDisableClientState(GL_VERTEX_ARRAY);
+        glfwSetKeyCallback(window, key_callback); //callback gombnyomÃ¡sra
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	glfwTerminate();
+    glfwTerminate();
 }
 
 
 /*int main(void) {
 
-	if (!glfwInit()) { exit(EXIT_FAILURE); }
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(600, 600, "Ablak sarga haterrel);
-	glfwMakeContextCurrent(window);
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Ablak sarga haterrel);
+    glfwMakeContextCurrent(window);
 
-	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
-	glfwSwapInterval(1);
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+    glfwSwapInterval(1);
 
-	init(window);
+    init(window);
 
-	while (!glfwWindowShouldClose(window)) {
-		display(window, glfwGetTime());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);*/
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);*/
